6935a478ec5a5a80ac81f4e631451ee3a6314e89
yiaf <tendencias323@gmail.com>
2022-07-17 23:37:38 -0500

    Update programa019.py

--- programa019.py
+++ programa019.py
@@ -1,7 +1,7 @@
 #https://github.com/yiaf/pythonbasico
 #Crea una lista numeros y recorre buscando el mayor
 #Recuerde la posición incial de una lista es 0
-listado = [100,50,60,70,37,1,0,200,201,90,99,701,100,101]
+listado = [90,100,50,60,70,37,25,1,0,200,201,90,99,701,100,101]
 mayor = listado[0]
 n = len(listado)
 i=0
