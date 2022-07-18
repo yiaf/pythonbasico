b38714362d77fe01d2358ca351f361ad70a1b107
yiaf <tendencias323@gmail.com>
2022-07-17 23:21:13 -0500

    Busca el mayor

---
+++ programa019.py
@@ -0,0 +1,14 @@
+#https://github.com/yiaf/pythonbasico
+#Crea una lista numeros y recorre buscando el mayor
+#Recuerde la posición incial de una lista es 0
+listado = [100,50,60,70,37,1,0,200,201,90,99,701,100]
+mayor = listado[0]
+n = len(listado)
+i=0
+while i < n-1:
+    i+=1
+    if mayor < listado[i]:
+        mayor = listado[i]
+        pos= i
+print("El mayor es: ",mayor, "En la posicion ",pos)
+    
