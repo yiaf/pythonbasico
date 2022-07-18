e05377bfa5db377ca11a15563fdadea0d90a281e
yiaf <tendencias323@gmail.com>
2022-07-17 21:48:51 -0500

    Solo modifcado comentario proy18

--- programa018.py
+++ programa018.py
@@ -1,5 +1,6 @@
 #https://github.com/yiaf/pythonbasico
 #Crea una lista de nombres y realiza un recorrido
+#Recuerde la posición incial de una lista es 0
 listado = ['RICARDO','MARÍA','PEDRO','SUSANA','MARCO','BEATRIZ','OTRO']
 i = 0
 n = len(listado)
